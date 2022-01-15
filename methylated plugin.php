284cfd468b36c009aa1c020bb8cdc44bc862f1a6
Ampeire <ampeireedgar5@gmail.com>
2022-01-15 09:38:23 +0300

    Initial Commit

---
+++ methylated plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package MethylatedPlugin
+ */
+/*
+ Plugin Name: Methylated Plugin
+ PLugin URL: http://ampeire.com
+Description: This is my first Wordpress plugin
+Version: 1.0.0
+Author: Ampeire Edgar
+Author URI:
+License: GPLv2 or later
+Text Domain: methylated-plugin
+ */
